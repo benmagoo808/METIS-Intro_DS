c485dc1ba2b6ce00d937056ccf71b3a6a13693ac
Ben <benmagoo@gmail.com>
2017-09-27 15:50:52 -0700

    Final answer of self directed code

--- most_common_word.py
+++ most_common_word.py
@@ -43,8 +43,8 @@ def get_most_common(word_list):
 
 
 word_list = get_clean_word_list()
-most_common, freq = get_most_common(word_list)
+most_common_word, frequency = get_most_common(word_list)
 # Print output
 print('The word(s) which appear the most are: ')
-print(most_common.title())
-print('Appearing ' + str(freq) + ' times')
+print(most_common_word.title())
+print('Appearing ' + str(frequency) + ' times')
